6f75d69c15d6c33a363e4b395700a84887bb4c72
limjoobin <joobin.lim97@gmail.com>
2021-03-16 22:10:48 +0800

    Added HomePage and some CSS Styling

---
+++ src/components/HomePage.vue
@@ -0,0 +1,21 @@
+<template> 
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'HomePage',
+    data (){
+        return{}
+    }
+
+}
+</script>
+
+
+<style>
+
+</style>
