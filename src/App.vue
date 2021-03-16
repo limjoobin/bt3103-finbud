7b858f9acca7d038beaa961bcc3588cb4b42541a
limjoobin <joobin.lim97@gmail.com>
2021-03-16 22:09:40 +0800

    Added default view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <div id = "app">
+	<router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data (){
+    return{}
   }
+
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @font-face {
+        font-family: Lustria;
+        src: url('assets/Lustria-Regular.ttf');
+    }
+    @font-face {
+        font-family: Lato;
+        src: url('assets/Lato-Regular.ttf');
+    }
+    *{
+        background-color: #a9d6ff;
+        color:#0E4070;
+        margin: 0px;
+        padding: 0px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Routes from './routes.js'
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+const myRouter = new VueRouter({
+  routes:Routes,
+  mode:'history'
+});
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: myRouter,
 }).$mount('#app')
