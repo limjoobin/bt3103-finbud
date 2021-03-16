0334e50a7cd50fac503a09e7c9fb50d0237fbeff
limjoobin <joobin.lim97@gmail.com>
2021-03-16 22:57:52 +0800

    Added routing for After Login page

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import HomePage from './components/HomePage.vue'
+import AfterLogin from './components/AfterLogin.vue'
 
 export default[
-    {path:'/', component: HomePage}
+    {path:'/', component: HomePage},
+    {path:'/dashboard', component: AfterLogin}
 ]
