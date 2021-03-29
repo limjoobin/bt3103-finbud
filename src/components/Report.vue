3fdda4a71967ad4cb86b22b4c08ccb66d7f27d66
dljw <darrenljw96@gmail.com>
2021-03-29 16:34:13 +0800

    Added report

---
+++ src/components/Report.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <retirement-chart :dataset="dataset"></retirement-chart>
+    </div>    
+</template>
+
+
+<script>
+import RetirementChart from './charts/RetirementCharts/RetirementChart.vue'
+
+export default {
+  components: { RetirementChart },
+    name:"report",
+    props:["dataset"],
+    data:function(){
+        return{
+
+        }
+    },created(){
+        console.log(this.dataset)
+    }, watch:{
+        dataset: {
+            handler: function (val, oldVal) {
+                console.log('watch 1', 'newval: ', val, '   oldVal:', oldVal)
+            },
+            deep: true
+        }
+    }
+}
+</script>
