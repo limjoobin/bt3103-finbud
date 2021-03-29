8dde6c4c2e3a45f4d941a5768ea91dde38470f5f
KokYiLing <kyiling0012@gmail.com>
2021-03-29 17:28:01 +0800

    Added initial homepage and login

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="main">
+    <img alt="Vue logo" src="../assets/logo.png" height="70px" />
+    <h1>Welcome to FinBud!</h1>
+    <h3>The only personal finance buddy you need</h3>
+    <div class="register">
+      <h3>Create your FinBud account</h3>
+      <form id="reg-form">
+        <div>
+          <label for="name">Name:</label>
+          <input type="text" id="name" placeholder="Enter Fullname" />
+        </div>
+        <div>
+          <label for="email">Email:</label>
+          <input type="text" id="email" placeholder="Enter Email" v-model="email"/>
+        </div>
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" id="username" placeholder="Enter Your Username" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" id="password" placeholder="Enter Password" v-model="password"/>
+        </div>
+        <div>
+          <label for="password-again">Password Again:</label>
+          <input
+            type="password"
+            id="password-again"
+            placeholder="Re-enter Password"
+          />
+        </div>
+        <br />
+        <div>
+          <label></label>
+          <button type="submit" id="signUp" v-on:click="register()">Sign up
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "../firebase"
+export default {
+  data() {
+    return {
+    email:"",
+    password:"",
+    }
+  },
+methods: {
+  register() {
+    alert(this.email + ":" + this.password);
+    firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(() => {
+        alert('Successfully registered! Please login.');
+        this.$router.push('/');
+      })
+      .catch(error => {
+        alert(error.message);
+      });
+  },
+},
+};
+</script>
+
+<style scoped>
+#main {
+  background-color: #a9d6ff;
+  height: 600px;
+}
+.register {
+  background-color: white;
+  width: 500px;
+  margin: 10px auto;
+  padding: 10px;
+  border: 7px solid green-border;
+  border-radius: 10px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  color: #444;
+  background-color: back-color;
+  box-shadow: 0 0 20px 0 #000000;
+  height: 350px;
+}
+
+label {
+  display: inline-block;
+  width: 25%;
+  text-align: right;
+  margin: 10px;
+}
+
+input[type="text"],
+input[type="password"] {
+  width: 55%;
+  padding: 5px;
+  font-size: 0.9em;
+  border-radius: 5px;
+  background: rgba(0, 0, 0, 0.07);
+}
+.button {
+  width: 160px;
+  height: 40px;
+  border-radius: 10px;
+  border: 10px solid #a9b6ff;
+  background-color: #a9b6ff;
+  color: white;
+  font-size: 15px;
+  text-align: center;
+}
+#signUp {
+    width: 160px;
+  height: 40px;
+  border-radius: 10px;
+  border: 10px solid #a9b6ff;
+  background-color: #a9b6ff;
+  color: white;
+  font-size: 15px;
+  text-align: center;
+}
+</style>
---
+++ src/components/welcome.vue
@@ -0,0 +1,65 @@
+<template>
+<div id="welcome">
+    <img alt="Vue logo" src="../assets/logo.png" height="70px">
+    <h1>Welcome to FinBud!</h1>
+    <h3>The only personal finance buddy you need</h3>
+    <div class="flex-container">
+        <div class="one">
+        <img class="cropped" src="../assets/mainpic.png" height="360px">
+        </div>
+
+        <div class="two">
+            <div id="login"><login></login></div>
+        </div>
+
+
+    </div>
+</div>
+    
+</template>
+
+
+<script>
+import login from './login.vue'
+
+
+export default {
+    components: {
+        login
+    }
+
+    }
+    
+
+</script>
+
+<style scoped>
+h3,h1 {
+    font-family: Lustria;
+
+}
+
+.flex-container {
+    display: flex;
+    height: 400px;
+}
+
+.one {
+    width:70%;
+    float: left;
+
+}  
+
+.flex-child:first-child {
+    margin-right: 10px;
+
+}
+
+#welcome {
+    background-color: #A9D6FF;
+}
+
+#login {
+    margin: auto;
+}
+</style>
