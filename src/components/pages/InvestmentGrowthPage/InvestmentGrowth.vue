7a81b01070c7158c457158a7cd54389c58a257ff
leow yong sen <80029786+twyongelve@users.noreply.github.com>
2021-03-30 01:36:18 +0800

    Fix logic error for chart calculation

--- src/components/pages/InvestmentGrowthPage/InvestmentGrowth.vue
+++ src/components/pages/InvestmentGrowthPage/InvestmentGrowth.vue
@@ -1,8 +1,9 @@
 <template>
     <div style ="background-color: #B9DEFF;">
+        <header1></header1>
         <img class ="timer" v-bind:src="img">
         <p class ="b1">{{content}}</p>
-        
+        <br><br>
         <div class ="budCon1">
             <table class ="yourBudget">
                 <tr class ="tr1">
@@ -27,10 +28,15 @@
                      </th>
                     <td class ="test">
                         <p style = "margin: 0px; font-size: 20px;">Investment Value:</p>
-                        <p style = "font-size: 30px">${{value1}}
+                        <br>
+                        <p style = "font-size: 30px">${{value1}}</p>
+                        <br>
                         <p style = "margin: 0px; font-size: 20px">Contribution:</p>
-                        <p style = "font-size: 30px">${{value2}}
+                        <br>
+                        <p style = "font-size: 30px">${{value2}}</p>
+                        <br>
                         <p style = "margin: 0px; font-size: 20px;">Profit:</p>
+                        <br>
                         <p style = "font-size: 30px">${{value3}}</p>
                     </td>
                 </tr>
@@ -51,60 +57,23 @@
                     </td>
                 </tr>
             </table>
+            <br><br><br><br>
             <igchart :data="data" :options="chartOptions"/>
         </div>
+        <br><br><br><br>
         <div class ="budCon2">
             <p style ="text-align: center; padding-top: 10px;font-size: 50px; color: #0E4070">You might be interested in...</p>
-            <button class ="navBut" @click="$router.push('/')">Find out your Ideal Budget for Investment</button>
-            <button class ="navBut" @click ="$router.push('/CommunityContent')">Find out how long can your investment last you for</button>
+            <br>
+            <button class ="navBut" @click="$router.push('/budget_planning')">Find out your Ideal Budget for Investment</button>
+            <button class ="navBut">Find out how long can your investment last you for</button>
             <button class ="navBut">Map out your pathway to Financial Freedom</button>
         </div>
-        <!--
-        <table class ="yourBudget">
-          <tr>
-            <p style = "text-align:center; font-size: 20px; color: #0E4070">Your Budget</p>
-            <td>
-              <p style = "margin: 1px; font-size: 20px; margin-left: 20px; color: #0E4070">How much you have for:</p>
-            </td>
-          </tr>
-          <tr>
-            <th style = "position: relative; bottom: 140px; left: 15%; margin-bottom: 30px; color: #0E4070; font-size: 20px">Your take home pay:
-                <p></p>
-                <input type="number" id="amount" style = "width:45%; height: 30px; border-radius: 5px; background-color: #D7ECFF" required>
-            </th>
-            <td class ="test">
-              <p style = "margin: 0px; font-size: 20px;">Necessities:</p>
-              <p style = "font-size: 30px">${{value1}}
-              <p style = "margin: 0px; font-size: 20px">Wants:</p>
-              <p style = "font-size: 30px">${{value2}}
-              <p style = "margin: 0px; font-size: 20px;">Savings and investing:</p>
-              <p style = "font-size: 30px">${{value3}}</p>
-            </td>
-          </tr>
-          <tr>
-            <th style = "text-align:center">
-                <b>The 50/30/20 budget rule:</b>
-                <br>
-                <label style ="font-size: 10px;">Senator Elizabeth Warren popularized the so-called </label>
-                <br>
-                <label style ="font-size: 10px;">50/30/20 budget rule" in her book, All Your Worth: </label>
-                <label style ="font-size: 10px;">The Ultimate Lifetime Money Plan. The basic rule is </label>
-                <br>
-                <label style ="font-size: 10px;">to divide up after-tax income and allocate it to spend: </label>
-                <br>
-                <label style ="font-size: 10px;">50% on needs, 30% on wants, and socking away 20% to savings.</label>
-            </th>
-            <td>
-              <p style = "margin: 0px; font-size: 10px;">Login or Signup now to save your progress!</p>
-            </td>
-          </tr>
-        </table>
-        -->
     </div>
 </template>
 
 <script>
 import IGChart from '../../charts/IGChart'
+import HeaderIG from './HeaderIG.vue'
 
 export default {
     name: 'InvestmentGrowth',
@@ -123,8 +92,8 @@ export default {
         }
     },
     methods: {
-        investGrowth: function() {
-            this.$router.push('/investment_growth'); 
+        budgetCalulator: function() {
+            this.$router.push('/budget_planning'); 
         },
         community: function() {
             this.$router.push('/community');
@@ -157,14 +126,16 @@ export default {
             var tracker = Number(this.startingBalance);
             var storage = [];
             storage.push(tracker);
-            for (let i = 0; i < this.Duration*4; i++) {
-                tracker += Number(this.Pa);
-                storage.push(tracker);
-                if (i == 0) {
-                    total += total*Number(this.Arr/400);
+            for (let i = 0; i < this.Duration; i++) {
+                for (let j = 0; j < 4; j++) {
+                    tracker += Number(this.Pa);
+                    storage.push(tracker);
+                    if (i == 0) {
+                        total += total*Number(this.Arr/400);
+                    }
+                    total += Number(this.Pa);
+                    total += (Number(total)*Number(this.Arr/400));
                 }
-                total += Number(this.Pa);
-                total += (Number(total)*Number(this.Arr/400));
                 this.data.push(total);
             }
             this.value1 = this.data.slice(-1)[0];
@@ -179,14 +150,16 @@ export default {
             var tracker = Number(this.startingBalance);
             var storage = [];
             storage.push(tracker);
-            for (let i = 0; i < this.Duration*12; i++) {
-                tracker += Number(this.Pa);
-                storage.push(tracker);
-                if (i == 0) {
-                    total += total*Number(this.Arr/1200);
-                }
-                total += Number(this.Pa);
-                total += (Number(total)*Number(this.Arr/1200));
+            for (let i = 0; i < this.Duration; i++) {
+                for (let j = 0; j < 12; j++) {
+                    tracker += Number(this.Pa);
+                    storage.push(tracker);
+                    if (i == 0) {
+                        total += total*Number(this.Arr/1200);
+                    }
+                    total += Number(this.Pa);
+                    total += (Number(total)*Number(this.Arr/1200));
+                }    
                 this.data.push(total);
             }
             this.value1 = this.data.slice(-1)[0];
@@ -201,14 +174,16 @@ export default {
             var tracker = Number(this.startingBalance);
             var storage = [];
             storage.push(tracker);
-            for (let i = 0; i < this.Duration*52; i++) {
-                tracker += Number(this.Pa);
-                storage.push(tracker);
-                if (i == 0) {
-                    total += total*Number(this.Arr/5200);
+            for (let i = 0; i < this.Duration; i++) {
+                for (let j = 0; j < 52; j++) {
+                    tracker += Number(this.Pa);
+                    storage.push(tracker);
+                    if (i == 0) {
+                        total += total*Number(this.Arr/5200);
+                    }
+                    total += Number(this.Pa);
+                    total += (Number(total)*Number(this.Arr/5200));
                 }
-                total += Number(this.Pa);
-                total += (Number(total)*Number(this.Arr/5200));
                 this.data.push(total);
             }
             this.value1 = this.data.slice(-1)[0];
@@ -242,6 +217,7 @@ export default {
     },
     components: {
         'igchart': IGChart,
+        header1: HeaderIG,
     },
 }
 </script>
@@ -263,7 +239,7 @@ export default {
     height: 40px;
     width: 40px;
     position: relative;
-    top: 45px;
+    top: 30px;
     left: 290px;
 }
 
@@ -271,6 +247,7 @@ export default {
     font-size: 15px;
     font-family: Lato;
     margin-left: 340px;
+    background-color: #B9DEFF;
 }
 
 .yourBudget{
