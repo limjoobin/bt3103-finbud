3691c762199917d5732b931bb91113eefc22e718
limjoobin <joobin.lim97@gmail.com>
2021-04-05 18:11:59 +0800

    Refactored Subfooter from AfterLogin

--- src/components/pages/AfterLogin/AfterLogin.vue
+++ src/components/pages/AfterLogin/AfterLogin.vue
@@ -3,16 +3,12 @@
         <h1>
             Good Morning {{ username }}!<br>
             What would you like to do today?
-           
         </h1>
-        <button class ="logout" v-on:click="logout()" style = "background-color: white;font-size: 30px; margin-left: 80%;">Logout</button>
+
+        <button class ="logout" v-on:click="logout()" >Logout</button>
         <dashboard-buttons></dashboard-buttons>
-        <div id = 'sub-footer'>
-            <h1>Exclusive Financial Deals</h1>
-            <button class='financial-deals'>Credit Card Promotion</button>
-            <button class='financial-deals'>Comprehensive Insurance Coverage</button>
-            <button class='financial-deals'>High Yield Investment Returns</button>
-        </div>
+        <subfooter></subfooter>
+        
     </div>
     
 </template>
@@ -20,6 +16,7 @@
 <script>
 import firebase from "../../../../firebase";
 import DashboardButtons from "./DashboardButtons.vue"
+import Subfooter from "./Subfooter.vue"
 
 export default {
     name: 'AfterLogin',
@@ -30,6 +27,7 @@ export default {
     },
     components:{
         'dashboard-buttons' : DashboardButtons,
+        'subfooter' : Subfooter
     },
     methods: {
         logout: function () {
@@ -46,10 +44,8 @@ export default {
             });
         },
     }
-
 }
 </script>
-
 
 <style>
     h1{
@@ -58,33 +54,9 @@ export default {
         margin: 20px;
         line-height: 1.5;
     }
-   
-    #sub-footer{
+    .logout{
         background-color: white;
-        width: 100%;
-        margin-top: 20px;
-        padding-bottom:20px;
-        text-align: center;
+        font-size: 30px; 
+        margin-left: 80%;
     }
-    #sub-footer h1{
-        background-color: white;
-        padding: 20px;
-    }
-    .financial-deals{
-        background-color: white;
-        background-image:url('https://cdn.pling.com/img/d/c/0/2/e4fff450a6306e045f5c26801ce31c3efaeb.jpg');
-        background-repeat: no-repeat;
-        background-size: contain;
-        background-position: center;
-        border:none;
-        text-align:center;
-        display: table-cell;
-        width: 20%;
-        min-width: 20%;
-        padding: 10% 0 20 0;
-
-        font-family: Lato;
-        font-size: 20px;
-    }
-
 </style>
---
+++ src/components/pages/AfterLogin/Subfooter.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id = 'sub-footer'>
+        <h1>Exclusive Financial Deals</h1>
+        <button class='financial-deals'>Credit Card Promotion</button>
+        <button class='financial-deals'>Comprehensive Insurance Coverage</button>
+        <button class='financial-deals'>High Yield Investment Returns</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{}
+    },
+}
+</script>
+
+<style>
+    #sub-footer{
+        background-color: white;
+        width: 100%;
+        margin-top: 20px;
+        padding-bottom:20px;
+        text-align: center;
+    }
+    #sub-footer h1{
+        background-color: white;
+        padding: 20px;
+    }
+    .financial-deals{
+        background-color: white;
+        background-image:url('https://cdn.pling.com/img/d/c/0/2/e4fff450a6306e045f5c26801ce31c3efaeb.jpg');
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center;
+        border:none;
+        text-align:center;
+        display: table-cell;
+        width: 20%;
+        min-width: 20%;
+        padding: 10% 0 20 0;
+
+        font-family: Lato;
+        font-size: 20px;
+    }
+</style>
