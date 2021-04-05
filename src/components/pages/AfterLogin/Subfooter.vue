33d4271da23921e2b745f43ccc15d216b035fa2c
limjoobin <joobin.lim97@gmail.com>
2021-04-05 18:52:51 +0800

    Changed UI of subfooter in AfterLogin

--- src/components/pages/AfterLogin/Subfooter.vue
+++ src/components/pages/AfterLogin/Subfooter.vue
@@ -1,9 +1,9 @@
 <template>
     <div id = 'sub-footer'>
         <h1>Exclusive Financial Deals</h1>
-        <button class='financial-deals'>Credit Card Promotion</button>
-        <button class='financial-deals'>Comprehensive Insurance Coverage</button>
-        <button class='financial-deals'>High Yield Investment Returns</button>
+        <button id='credit-card-promo'>Credit Card Promotion</button>
+        <button id='insurance'>Comprehensive Insurance Coverage</button>
+        <button id='investment'>High Yield Investment Returns</button>
     </div>
 </template>
 
@@ -25,22 +25,33 @@ export default {
     }
     #sub-footer h1{
         background-color: white;
-        padding: 20px;
+        padding: 2% 0 0 0 ;
+        margin:0;
     }
-    .financial-deals{
+    #sub-footer button{
         background-color: white;
-        background-image:url('https://cdn.pling.com/img/d/c/0/2/e4fff450a6306e045f5c26801ce31c3efaeb.jpg');
         background-repeat: no-repeat;
-        background-size: contain;
+        background-size: 80%;
         background-position: center;
         border:none;
         text-align:center;
         display: table-cell;
         width: 20%;
         min-width: 20%;
-        padding: 10% 0 20 0;
+        padding: 20% 0 0 0;
 
         font-family: Lato;
         font-size: 20px;
+    }
+
+
+    #credit-card-promo{
+        background-image:url('../../../assets/credit-card.png');
+    }
+    #insurance{
+        background-image:url('../../../assets//insurance.png');
+    }
+    #investment{
+        background-image:url('../../../assets/investment.png');
     }
 </style>
